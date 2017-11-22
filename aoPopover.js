4005b5a4219d05741a1c1eee75ebedcee2312d4b
longmeaghan <long.meaghan.e@gmail.com>
2017-11-21 17:10:48 -0800

    new feature popovers done

--- aoPopover.js
+++ aoPopover.js
@@ -1,29 +1,37 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+  console.log("ready!");
 });
 
 //information popover TOP
 $(".openInformTop").click(function() {
- $(".informPopoverTop").addClass("visible");
+  $(".informPopoverTop").addClass("visible");
 });
 $(".closeInformTop").click(function() {
   $(".informPopoverTop").removeClass("visible");
   $(".informPopoverTop").addClass("end");
 });
 
+$(".openInformBottom").click(function() {
+  $(".informPopoverBottom").addClass("visible");
+});
+$(".closeInformBottom").click(function() {
+  $(".informPopoverBottom").removeClass("visible");
+  $(".informPopoverBottom").addClass("end");
+});
+
 //newfeature popover
-$(".gotitTop").click(function(){
+$(".gotitTop").click(function() {
   $(".nfPopoverTop").addClass("hidden");
 });
 
-$(".gotitBottom").click(function(){
+$(".gotitBottom").click(function() {
   $(".nfPopoverBottom").addClass("hidden");
 });
 
-$(".gotitRight").click(function(){
+$(".gotitRight").click(function() {
   $(".nfPopoverRight").addClass("hidden");
 });
 
-$(".gotitLeft").click(function(){
+$(".gotitLeft").click(function() {
   $(".nfPopoverLeft").addClass("hidden");
 });
